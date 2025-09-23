10205052fb1182fd4142299093cb83fef4691322
Adham Rashidov <adhamrashidov@Adhams-MacBook-Pro.local>
2025-09-23 18:38:43 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
+
+@Module({
+	imports: [UserModule, PrismaModule, PostModule]
+})
+export class AppModule { }
