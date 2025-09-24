c79a0e79b2b6267a30a328821c023bc3b0c9620d
Adham Rashidov <adhamrashidov@Adhams-MBP.bbrouter>
2025-09-24 22:29:40 +0500

    graphQl

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -2,6 +2,9 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Post {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+	@Field(() => Int)
+	id?: number;
+
+	@Field()
+	title?: string;
 }
